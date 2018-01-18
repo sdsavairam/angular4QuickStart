913d2eff4f8730468708abf1d1b6e1ce4754dc62
Savairam <sagar.savairam@capgemini.com>
2018-01-18 20:26:38 +0530

    Dashboard view added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,23 +7,18 @@ import {HeroService} from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  /*hero: Hero = {
-    id:1,
-    name: 'Windstorm'
-  }*/ 
-  heroes : Hero[];
-  selectedHero : Hero;
-  onSelect(hero:Hero): void {
-    this.selectedHero = hero;
-  }
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
-  }
-  constructor(private heroService : HeroService) { }
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
